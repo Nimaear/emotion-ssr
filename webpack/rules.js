482ad59668ddc05ec737bc0041ae82095ad189fa
Freedom Fighter <ozgur.gene@gmail.com>
2018-10-17 15:59:32 +0200

    Remove more stuff

--- webpack/rules.js
+++ webpack/rules.js
@@ -1,23 +1,12 @@
-const ExtractCssChunks = require('extract-css-chunks-webpack-plugin');
-const createMinifier = require('css-loader-minify-class');
-
-const isDev = process.env.NODE_ENV;
-
-
 const babelLoader = {
   test: /\.js$/,
   exclude: /node_modules/,
   use: 'babel-loader',
 };
 
+const clientDev = [babelLoader];
 
-const clientDev = [
-  babelLoader,
-];
-
-const serverDev = [
-  babelLoader,
-];
+const serverDev = [babelLoader];
 
 module.exports = {
   client: {
